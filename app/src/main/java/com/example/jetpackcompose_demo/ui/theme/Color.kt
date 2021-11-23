2176ecb9ad4642758b2e25990c6fdfad75454290
shan1998 <shan.2018477@iit.ac.lk>
2021-11-23 11:20:00 +0530

    first commit

---
+++ app/src/main/java/com/example/jetpackcompose_demo/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package com.example.jetpackcompose_demo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MainColor = Color(0xff5a189a)
---
+++ app/src/main/java/com/example/jetpackcompose_demo/ui/theme/Type.kt
@@ -0,0 +1,26 @@
+package com.example.jetpackcompose_demo.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcompose_demo.R
+
+
+//Set Fonts
+private val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_light, weight = FontWeight.Light),
+    Font(R.font.poppins_bold, weight = FontWeight.Bold),
+    Font(R.font.poppins_black, weight = FontWeight.Black),
+    Font(R.font.poppins_italic, style = FontStyle.Italic),
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    defaultFontFamily = Poppins
+    )
+
